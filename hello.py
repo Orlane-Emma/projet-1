87cfd7894b2f6a05b7decf0622580c346a60b43b
Andrea Orlane <eitchiri@gmail.com>
2024-01-31 13:42:16 +0100

    afficher les multiples

--- hello.py
+++ hello.py
@@ -1,4 +1,12 @@
 nombre = int(input("Entrez un nombre : "))
 print(nombre)
 
+def afficher_multiples() :
+    for i in range(1,11) :
+        result = i*nombre 
+        print(f"{i} * {nombre} = {result}")
+        
+afficher_multiples()
+
+        
 
